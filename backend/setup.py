2d19245a4c7e84e4b3b1fc56aeb6841d4d79f646
Steffen Allner <sa@gocept.com>
2016-05-18 13:20:03 +0200

    PEP-8

--- backend/setup.py
+++ backend/setup.py
@@ -13,36 +13,37 @@ requires = [
     'pytest-sugar',
     'pytest-flake8',
     'pytest-cov',
-    ]
+]
 
 test_requires = [
     'pytest',
-    ]
+]
 
-setup(name='schaukasten',
-      version='0.0',
-      description='schaukasten',
-      classifiers=[
+setup(
+    name='schaukasten',
+    version='0.0',
+    description='schaukasten',
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='gocept gmbh & co. kg',
-      author_email='mail@gocept.com',
-      url='https://github.com/gocept/schaukasten',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='schaukasten',
-      install_requires=requires,
-      extras_require={
+    ],
+    author='gocept gmbh & co. kg',
+    author_email='mail@gocept.com',
+    url='https://github.com/gocept/schaukasten',
+    keywords='web wsgi bfg pylons pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='schaukasten',
+    install_requires=requires,
+    extras_require={
         'test': test_requires,
-      },
-      entry_points={
+    },
+    entry_points={
         'paste.app_factory': [
             'main=schaukasten.browser.app:factory'
         ],
-      },
-      )
+    },
+)
